50e9976678ffba724bd20778f18473102cedf539
Mhmd_.akh <79569464+Mhmd-Akh@users.noreply.github.com>
2023-02-11 03:52:51 +0330

    Create test.js

---
+++ test.js
@@ -0,0 +1,11 @@
+const radio = require("harmonyradio")
+
+radio({
+    IdChannel: "1013186636718276628",
+    Links: "https://www.youtube.com/watch?v=93lXh1yBjpg",
+    Youtube: true,
+    Token: "",
+    GuildId: "578558255392096256",
+    ResetChannelId: "1044631776640901190",
+    LogId: "1013185293408538805"
+})
