6e5219d47e3579c4597c8f567de10de4a64a2ed2
Mhmd_.akh <79569464+Mhmd-Akh@users.noreply.github.com>
2023-02-17 04:12:03 +0330

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,16 @@
 const radio = require("radio-harmony")
 
 radio({
-    IdChannel: "1013186636718276628",
-    Links: "https://www.youtube.com/watch?v=93lXh1yBjpg",
-    Youtube: true,
+    IdChannel: "1068101090941337600",
+    Links: [
+        "https://www.youtube.com/watch?v=93lXh1yBjpg",
+        "https://open.spotify.com/track/0KtZ7AgzYRKYo3ckyJ3KBt?si=64228aab45d940ec",
+        "https://soundcloud.com/kaltrina-zejneli/billie-eilish-you-should-see-me-in-a-crown-1",
+        "https://www.youtube.com/watch?v=pvDPgVn3t60",
+        "https://soundcloud.com/mervinmowlley/qveen-herby-sugar-daddy-mervin-mowlll-remix",
+        "https://www.youtube.com/watch?v=3DlpotDDmLc",
+        "https://laut.fm/flr1"
+    ],
     Token: "",
     GuildId: "578558255392096256",
     ResetChannelId: "1044631776640901190",
